f36ebde8f3d75f075550bfe58aacdeb0fd1e825e
ashjoy7 <ashjoy7@gmail.com>
2024-05-21 20:01:41 -0700

    update

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,6 +1,14 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
+
+// Check if swaggerDocument exists
+if (!swaggerDocument) {
+    console.error('swagger-output.json not found');
+    process.exit(1); // Exit the process if swaggerDocument is not found
+}
+
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
