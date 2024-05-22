9516e95d66281494b21fe9ffe5ffdee8f642a8bf
ashjoy7 <ashjoy7@gmail.com>
2024-05-21 17:04:20 -0700

    update

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const session = require('express-session');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger-output.json');
 
@@ -17,25 +15,16 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Middleware
 app.use(bodyParser.json());
-app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Import routes
 const indexRoutes = require('./routes/index');
 const bookRoutes = require('./routes/bookRoutes');
-const authRoutes = require('./routes/authRoutes');
 
 // Use routes
 app.use('/', indexRoutes);
 app.use('/api', bookRoutes);
-app.use('/', authRoutes);
 
 // Serve Swagger docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-// Passport configuration
-require('./config/passport');
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
