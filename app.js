fb74282adac14e15ceeae4e807c21fcfdfbfc9da
ashjoy7 <ashjoy7@gmail.com>
2024-05-21 17:48:25 -0700

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Middleware
 app.use(bodyParser.json());
