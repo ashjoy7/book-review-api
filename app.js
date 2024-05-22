86cdecdbc9d832cf35af74db7baa160acd4a4392
ashjoy7 <ashjoy7@gmail.com>
2024-05-21 18:36:56 -0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger-output.json');
+const swaggerRoutes = require('./routes/swagger'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,7 +21,18 @@ mongodb.initDb((err) => {
 });
 
 // Parse JSON bodies
-app.use(bodyParser.json());
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    next(); // Call next() after setting headers
+  });
 
 // Define routes
 const indexRoutes = require('./routes/index');
