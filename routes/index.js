69ad569657172bbb1a2b44b080f64441c37d60e7
ashjoy7 <ashjoy7@gmail.com>
2024-05-21 20:14:16 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/swagger', require('./swagger'));
-router.use('/contacts', require('./bookRoutes'));
+router.use('/books', require('./bookRoutes'));
 router.use('/reviews', require('./reviewRoutes'));
 
 module.exports = router;
